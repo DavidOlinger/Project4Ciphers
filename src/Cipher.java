753d0cc42959976a6816ddfea5bc4cad1c88ff51
OLINGERDM22 <OlingerDM22@gcc.edu>
2023-04-17 12:17:56 -0400

    Added the stubs for the methods in Cipher

---
+++ src/Cipher.java
@@ -0,0 +1,23 @@
+public class Cipher {
+
+    public char encrypt(char c){
+        return c; //stub
+    }
+
+    public char decrypt(char c){
+        return c; //stub
+    }
+
+    public String encrypt(String s){
+        return s; //stub
+    }
+
+    public String decrypt(String s){
+        return s; //stub
+    }
+
+    public Cipher newCopy(){
+        return null; // stub
+    }
+
+}
